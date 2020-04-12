053f399b200fd29a4f405c77c98e072d8b20ed53
jgallego2377 <24864743+jgallego2377@users.noreply.github.com>
2020-04-12 00:35:31 -0700

    Create News.svc.cs

---
+++ News.svc.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace WebServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "News" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select News.svc or News.svc.cs at the Solution Explorer and start debugging.
+    public class News : INews
+    {
+        public List<NewsArticle> domainNews(string domain)
+        {
+            //cnbc, bbc-news, cbs-news, fox-news, cnn, reuters, abc-news, nbc-news
+            List<NewsArticle> objNewsData = new List<NewsArticle>();
+
+            string url = @"https://newsapi.org/v2/top-headlines?sources=" + domain + "&apiKey=f1e76ec74a5a49ddab8bd0b213bd6e3b";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader sreader = new StreamReader(dataStream);
+
+            string responsereader = sreader.ReadToEnd();
+            response.Close();
+
+            TopicNewsRootObject newsInfo = JsonConvert.DeserializeObject<TopicNewsRootObject>(responsereader);
+
+            int count = 0;
+            if (newsInfo.articles.Count > 5)
+                count = 5;
+            else
+                count = newsInfo.articles.Count;
+            
+            for (int i = 0; i < count; i++)
+            {
+                NewsArticle objNews = new NewsArticle();
+                objNews.url = newsInfo.articles[i].url;
+                objNews.author = newsInfo.articles[i].author;
+                objNews.title = newsInfo.articles[i].title;
+                objNews.publishedAt = newsInfo.articles[i].publishedAt;
+                objNewsData.Add(objNews);
+            }
+            return objNewsData;
+        }
+
+        public List<NewsArticle> topicNews(string topic)
+        {
+            List<NewsArticle> objNewsData = new List<NewsArticle>();
+
+            string url = @"https://newsapi.org/v2/top-headlines?language=en&q=" + topic + "&apiKey=f1e76ec74a5a49ddab8bd0b213bd6e3b";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader sreader = new StreamReader(dataStream);
+
+            string responsereader = sreader.ReadToEnd();
+            response.Close();
+
+            TopicNewsRootObject newsInfo = JsonConvert.DeserializeObject<TopicNewsRootObject>(responsereader);
+
+            int count = 0;
+            if (newsInfo.articles.Count > 5)
+                count = 5;
+            else
+                count = newsInfo.articles.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                NewsArticle objNews = new NewsArticle();
+                objNews.url = newsInfo.articles[i].url;
+                objNews.author = newsInfo.articles[i].author;
+                objNews.title = newsInfo.articles[i].title;
+                objNews.publishedAt = newsInfo.articles[i].publishedAt;
+                objNewsData.Add(objNews);
+            }
+            return objNewsData;
+        }
+
+        public List<NewsArticle> locationNews(string location)
+        {
+            List<NewsArticle> objNewsData = new List<NewsArticle>();
+
+            string url = @"https://newsapi.org/v2/top-headlines?country=" + location + "&apiKey=f1e76ec74a5a49ddab8bd0b213bd6e3b";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader sreader = new StreamReader(dataStream);
+
+            string responsereader = sreader.ReadToEnd();
+            response.Close();
+
+            TopicNewsRootObject newsInfo = JsonConvert.DeserializeObject<TopicNewsRootObject>(responsereader);
+
+
+            int count = 0;
+            if (newsInfo.articles.Count > 5)
+                count = 5;
+            else
+                count = newsInfo.articles.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                NewsArticle objNews = new NewsArticle();
+                objNews.url = newsInfo.articles[i].url;
+                objNews.author = newsInfo.articles[i].author;
+                objNews.title = newsInfo.articles[i].title;
+                objNews.publishedAt = newsInfo.articles[i].publishedAt;
+                objNewsData.Add(objNews);
+            }
+            return objNewsData;
+        }
+    }
+
+    public class TopicSource
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class TopicArticle
+    {
+        public TopicSource source { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public string urlToImage { get; set; }
+        public DateTime publishedAt { get; set; }
+        public string content { get; set; }
+    }
+
+    public class TopicNewsRootObject
+    {
+        public string status { get; set; }
+        public int totalResults { get; set; }
+        public List<TopicArticle> articles{ get; set; }
+    }
+}
